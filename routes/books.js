ea3ee37ff01f0cd465fdc60107e86cc176db1f11
Nicholas Peters <peters.nicholas@gmail.com>
2023-03-02 15:12:59 +0100

    addbook bugfix, and add isAvailable boolean

--- routes/books.js
+++ routes/books.js
@@ -6,20 +6,34 @@ const books = [
     id: 1,
     title: "The Hobbit",
     author: "J.R.R. Tolkien",
+    isAvailable: true,
   },
   {
     id: 2,
     title: "The Lord of the Rings",
     author: "J.R.R. Tolkien",
+    isAvailable: false,
   },
   {
     id: 3,
     title: "The Silmarillion",
     author: "J.R.R. Tolkien",
+    isAvailable: true,
   },
 ];
 
 router.get("/", function (req, res, next) {
+  res.send(books);
+});
+
+router.post("/addbook", function (req, res, next) {
+  const newBook = {
+    id: books.length + 1,
+    title: req.body.title,
+    author: req.body.author,
+    isAvailable: true,
+  };
+  books.push(newBook);
   res.send(books);
 });
 
